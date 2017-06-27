eeafaa3eeac799bf801f9ca66a484e599f0bb8e7
Izabella Baer <izabellabaer@gmail.com>
2017-06-27 14:34:27 -0700

    added necessary files, starting to retype lab11

---
+++ lab-izzy/lib/error-middleware.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = (err, req, res, next) => {
+  console.error(err.message);
+
+  // if validation error respond with 400
+  if(err.message.toLowerCase().includes('validation failed'))
+    return res.sendStatus(400);
+
+  // if duplicate key respond with 409
+  if(err.message.indexOf('duplicate key') > - 1)
+    return res.sendStatus(409);
+
+  // otherwise respond with 500
+  res.sendStatus(500);
+};
